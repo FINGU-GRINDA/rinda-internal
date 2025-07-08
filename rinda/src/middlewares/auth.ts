5e6f7be5c8e27680b4790400600449de41e90aec
vikyw89 <vikyw89@gmail.com>
2025-07-09 00:30:55 +0800

    feat: enhance auth and db middlewares to include PrismaClient and improve session handling

--- rinda/src/middlewares/auth.ts
+++ rinda/src/middlewares/auth.ts
@@ -1,17 +1,26 @@
 import { os } from "@orpc/server";
+import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+import { cookies, headers } from "next/headers";
+import { auth } from "@/lib/auth";
 import type { User } from "@/schemas/user";
 
 export const requiredAuthMiddleware = os
-	.$context<{ session?: { user?: User } }>()
+	.$context<{
+		session?: {
+			user: User;
+			session: { id: string; expiresAt: Date; token: string; userId: string };
+		} | null;
+	}>()
 	.middleware(async ({ context, next }) => {
 		/**
 		 * Why we should ?? here?
 		 * Because it can avoid `getSession` being called when unnecessary.
 		 * {@link https://orpc.unnoq.com/docs/best-practices/dedupe-middleware}
 		 */
+
 		const session = context.session ?? (await getSession());
 
-		if (!session.user) {
+		if (!session?.user) {
 			throw new Error("UNAUTHORIZED");
 		}
 
@@ -21,15 +30,9 @@ export const requiredAuthMiddleware = os
 	});
 
 async function getSession() {
-	/**
-	 * You can use headers or cookies here to create the user object:
-	 * import { cookies, headers } from 'next/headers'
-	 * const headerList = await headers();
-	 * const cookieList = await cookies();
-	 *
-	 * These lines are commented out because Stackblitz has issues with Next.js headers and cookies.
-	 * However, this works fine in a local environment.
-	 */
+	const [headerList] = await Promise.all([headers(), cookies()]);
 
-	return { user: { id: "unique", name: "unnoq", email: "contact@unnoq.com" } };
+	return await auth.api.getSession({
+		headers: headerList as ReadonlyHeaders,
+	});
 }
--- rinda/src/middlewares/db.ts
+++ rinda/src/middlewares/db.ts
@@ -1,23 +1,27 @@
 import { os } from "@orpc/server";
 import { QdrantClient } from "@qdrant/js-client-rest";
 import { envPrivate } from "@/privateEnv";
+import { PrismaClient } from "../../generated/prisma";
 
 export const dbProviderMiddleware = os
-	.$context<{ db?: QdrantClient }>()
+	.$context<{ qdrant?: QdrantClient; prisma?: PrismaClient }>()
 	.middleware(async ({ next }) => {
 		/**
 		 * Why we should ?? here?
 		 * Because it can avoid `createFakeDB` being called when unnecessary.
 		 * {@link https://orpc.unnoq.com/docs/best-practices/dedupe-middleware}
 		 */
-		const db = new QdrantClient({
+		const qdrant = new QdrantClient({
 			apiKey: envPrivate.QDRANT_API_KEY,
 			url: envPrivate.QDRANT_BASE_URL,
 		});
 
+		const prisma = new PrismaClient();
+
 		return next({
 			context: {
-				db,
+				qdrant,
+				prisma,
 			},
 		});
 	});
